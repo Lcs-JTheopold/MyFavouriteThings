e1709e808b9266100eba00791c0e008db625412a
Jakob Theopold <jtheopold@lcs.on.ca>
2024-12-11 11:14:24 -0500

    Initial Commit

---
+++ MyFavouriteThings/MyFavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavouriteThingsApp.swift
+//  MyFavouriteThings
+//
+//  Created by Jakob Theopold on 11.12.24.
+//
+
+import SwiftUI
+
+@main
+struct MyFavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
